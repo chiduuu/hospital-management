e9522417851120813663aed7290b71fb0afd338d
chiduuu <mrchidu@gmail.com>
2022-01-17 21:44:22 +0530

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,64 @@ import { GetDoctorComponent } from './doctor/get-doctor/get-doctor.component';
 import { CreatePatientComponent } from './patient/create-patient/create-patient.component';
 import { PatientListComponent } from './patient/patient-list/patient-list.component';
 import { GetPatientComponent } from './patient/get-patient/get-patient.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  // {
+  //   path: 'doctors', component: DoctorListComponent,
+  //   children: [
+  //     {
+  //       path:'create', component: CreateDoctorComponent
+  //     },
+  //     {
+  //       path: ':id', component: GetDoctorComponent
+  //     },
+  //     {
+  //       path: 'list', component: DoctorListComponent
+  //     }
+  // ]
+  // },
+  // {
+  //   path: 'patients', component: PatientListComponent,
+  //   children: [
+  //     {
+  //       path:'create', component: CreatePatientComponent
+  //     },
+  //     {
+  //       path: ':id', component: GetPatientComponent
+  //     },
+  //     {
+  //       path: 'list', component: PatientListComponent
+  //     }
+  // ]
+  // },
+  { path: 'doctors', component: DoctorListComponent },
+  {
+    path: 'doctor/create',
+    component: CreateDoctorComponent,
+  },
+  {
+    path: 'doctor/:id',
+    component: GetDoctorComponent,
+  },
+  {
+    path: 'patients',
+    component: PatientListComponent,
+  },
+  {
+    path: 'patient/create',
+    component: CreatePatientComponent,
+  },
+  {
+    path: 'patient/:id',
+    component: GetPatientComponent,
+  },
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   declarations: [
@@ -18,13 +76,10 @@ import { GetPatientComponent } from './patient/get-patient/get-patient.component
     GetDoctorComponent,
     CreatePatientComponent,
     PatientListComponent,
-    GetPatientComponent
+    GetPatientComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
